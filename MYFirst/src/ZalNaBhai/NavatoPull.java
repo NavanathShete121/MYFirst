aa6165dd321d6b1e188fd7174fc211ae1bb7b994
Navnath <navanathshete84@gmail.com>
2022-06-04 19:35:28 +0530

    commit

--- MYFirst/src/ZalNaBhai/NavatoPull.java
+++ MYFirst/src/ZalNaBhai/NavatoPull.java
@@ -1,5 +1,13 @@
 package ZalNaBhai;
 
 public class NavatoPull {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		System.out.println("Jaml Ki Mala");
+	}
+
 
 }
